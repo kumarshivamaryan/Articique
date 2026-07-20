3065b9d2f986104853c1ebb40a041212c93c3fe8
kumarshivamaryan <shivamkumar30092005@gmail.com>
2026-07-20 16:48:43 +0530

    feat: added backend Dockerfile and GitHub Actions CI pipeline

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
+const app = require('./src/app');
+
+const PORT = process.env.PORT || 3000;
 
 
-const app = require('./src/app')
-
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("server is running on port 3000");
 })
