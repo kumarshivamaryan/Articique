5724a03ae43ee264e10f55f6b6da7893d301a87b
kumarshivamaryan <shivamkumar30092005@gmail.com>
2026-07-20 15:43:33 +0530

    feat: added prometheus metrics and error simulation endpoints

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,10 +2,24 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const express = require('express');
+const client = require('prom-client');
 const aiRoutes = require('./routes/ai.routes')
 const cors = require('cors')
 
 const app = express();
+
+const register = new client.Registry();
+
+client.collectDefaultMetrics({ register });
+
+app.get('/metrics', async (req, res) => {
+  res.setHeader('Content-Type', register.contentType);
+  res.send(await register.metrics()); // Reads from Registry & Exports it
+});
+
+app.get('/simulate-error', (req, res) => {
+  res.status(500).send('💥 Simulated 500 Internal Server Error!');
+});
 
 app.use(cors())
 
